3b2c50be19ec6e5a43907b8f2aaa3609cc97257d
rajesh <rajaga223@gmail.com>
2021-08-13 16:47:21 +0530

    commit: react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Style/index.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      {console.log(data)}
+      <h1>welcome to react custom template </h1>
+    </div>
+  );
+};
+
+export default App;
